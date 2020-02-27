d44d382187d750e969236c48aa8bc9691023ac2e
Mars-or-bust <benwortman@gmail.com>
2020-02-27 11:50:46 -0500

    Prophet Benchmarking

--- Team 2/Team 2.R
+++ Team 2/Team 2.R
@@ -1,3 +1,17 @@
 install.packages("prophet")
 library("prophet")
 
+# set working directory to wherever your project is stored
+setwd("~/Desktop/Flaring Stars/Team 2")
+
+# test Pathnames
+test_num <- 1
+results_list <- c("000892713_results.Rdat", "001872210_results.Rdat", "001872078_results.Rdat")
+
+# Load in the lightcurve residuals
+lc = load(paste("../Results_Files_1/", results_list[test_num],sep = ""))
+lc = results$Lightcurve
+resid = results$ARFIMA$residuals
+
+
+
