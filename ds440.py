6a5962ca50f09fbc2a0328d1a22f827d61243b32
nickg014 <43478632+nickg014@users.noreply.github.com>
2020-02-27 10:05:39 -0500

    Add files via upload
    documentation/misc cleanup

--- ds440.py
+++ ds440.py
@@ -28,10 +28,10 @@ if platform.system() == 'Windows':
 else:
     fs = "/"
 dataFiles = [f for f in listdir(pathToData) if isfile(join(pathToData, f))]
-flares          =   []
-notFlares       =   []
+flares          =   []                                              #list of files flagged as flares by the R script
+notFlares       =   []                                              #list of files flagged as not flares
 workQueue       =   Queue()                                         #multithreading work queue       
-mutex           =   Lock()
+mutex           =   Lock()                                          #mutex lock for multithreading
 
 #----worker thread
 class RScriptWorker(Thread):
@@ -46,28 +46,28 @@ class RScriptWorker(Thread):
             file = self.queue.get()
             threadStart = timeit.default_timer()
             try:
-                #process the data file
+                #make the R command and let the R script process the data file
                 cmd = pathToR + " " + pathToRScript + " " + pathToData + fs + file
                 subOutput = subprocess.check_output(cmd).decode('UTF-8')
                 
-                #simulate random processing time (testing only)  
+                #simulate some random processing time (testing only)  
                 sleep(randint(2,10))
                 
-                #**************this part will need to change for the real R script. right now it only checks: output of 1 = flare 0 = not
+                #right now it only checks output of R script for: 1 = flare 0 = not
                 mutex.acquire()
                 if str(subOutput[4]) == '1':
                     flares.append(file)
                 else:
                     notFlares.append(file)
-                    
+                mutex.release()    
                 print("Processing file: {} ({} of {}) completed in {} seconds".format(file, len(flares)+len(notFlares), len(dataFiles),"%0.2f" % (timeit.default_timer() - threadStart)))
-                mutex.release()
+                
             finally:
                 self.queue.task_done()
 #--------------
 
 #start the processing
-print("Running DS440 Flare Stars Script v0.4")
+print("Running DS440 Flare Stars Script v0.5")
 print("Threads: ", threadCount)
 print("Run start: ", time)
 startTime = timeit.default_timer()
